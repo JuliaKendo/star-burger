50c67d780ff86771d1f63cb2bc36aad492242bef
root <ya1814@yandex.ru>
2022-03-17 09:49:12 +0000

    add interrupt by error

--- deploy_starburger.sh
+++ deploy_starburger.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
+set -e
+
 work_folder="/opt/star-burger"
 rollbar_token="218fcc9768274497bbb483d548e5b9d2"
 rollbar_env="production"
 
-set -e
 source $work_folder/venv/bin/activate
 
 apt-get -qq -y install httpie> /dev/null
@@ -12,16 +13,11 @@ apt-get -qq -y install httpie> /dev/null
 /usr/bin/git -C $work_folder pull --quiet
 commit_hash=$(git rev-parse --short HEAD)
 
-if [ $? -ne 0 ]; then
-  http --verify=no POST https://api.rollbar.com/api/1/deploy X-Rollbar-Access-Token:$rollbar_token \
-    environment=$rollbar_env \
-    revision=$commit_hash \
-    local_username=$(logname) \
-    status="failed"> /dev/null
-
-  echo "deploy aborted"
-  exit
-fi
+http --verify=no POST https://api.rollbar.com/api/1/deploy X-Rollbar-Access-Token:$rollbar_token \
+  environment=$rollbar_env \
+  revision=$commit_hash \
+  local_username=$(logname) \
+  status="failed"> /dev/null
 
 yes | pip install -r $work_folder/requirements.txt --quiet
 
@@ -33,14 +29,18 @@ npx browserslist@latest --update-db> /dev/null
 parcel build bundles-src/index.js --no-minify -d bundles --public-url="./"> /dev/null
 
 if [ ! -d $work_folder/staticfiles ]; then mkdir $work_folder/staticfiles; fi
-yes "yes" | python $work_folder/manage.py collectstatic> /dev/null
-yes "yes" | python $work_folder/manage.py migrate foodcartapp> /dev/null
-yes "yes" | python $work_folder/manage.py migrate> /dev/null
 
-systemctl restart starburger
+if [ $? -ne 0 ]; then
+  yes "yes" | python $work_folder/manage.py collectstatic> /dev/null
+  yes "yes" | python $work_folder/manage.py migrate foodcartapp> /dev/null
+  yes "yes" | python $work_folder/manage.py migrate> /dev/null
+
+  echo "deploy aborted"
+  exit
+fi
+
 systemctl reload nginx
-systemctl restart certbot-renewal.timer
-systemctl restart starburger-clearsessions.timer
+systemctl restart starburger certbot-renewal.timer starburger-clearsessions.timer
 
 http --verify=no POST https://api.rollbar.com/api/1/deploy X-Rollbar-Access-Token:$rollbar_token \
   environment=$rollbar_env \
